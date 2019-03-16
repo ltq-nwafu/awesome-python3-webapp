48e77464812952960e220361baa1e6f90a3da6c2
ltq <ltq@nwafu.edu.cn>
2019-03-16 20:47:05 +0800

    day05 update config files

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+# _*_ coding: uft-8 _*_
+
+"""
+Default configurations.
+"""
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '123456',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+
+"""
+Override configurations.
+"""
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
